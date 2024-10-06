d99a4b6e508401ad40f7b22ecf4804b281304421
Amit Barai <155969547+Amit-Barai123@users.noreply.github.com>
2024-10-06 01:35:19 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+import 'dotenv/config';  // Automatically loads and configures dotenv
+import 'babel-register'; // Allows you to use Babel to transpile ES6 on the fly
+import './server.js';       // Imports and runs the server
---
+++ page/page.modal.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const Page = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 25,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    content: Object,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model('Pages', Page);
